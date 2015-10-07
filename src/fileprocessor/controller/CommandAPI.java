f2e40cf85edba87bb4fc483570654aa4525ae77d
David Livet <david.livet@orange.fr>
2015-10-07 09:34:42 -0400

    CommandAPI add queue

--- src/fileprocessor/controller/CommandAPI.java
+++ src/fileprocessor/controller/CommandAPI.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import fileprocessor.model.FileNameCommand;
 import fileprocessor.model.ICommand;
@@ -12,10 +13,17 @@ import fileprocessor.model.ICommand;
 public class CommandAPI  {
 	private Map<String, Class<? extends ICommand>> commands;
 	private Queue<ICommand> commandQueue;
+	private boolean invokerRunning;
+	private InvokerThread thread;
+
 	public CommandAPI() {
 		 commands = new HashMap<String, Class<? extends ICommand>>();
-		 commandQueue = new LinkedList< ICommand>();
-		 
+		 commandQueue = new ConcurrentLinkedQueue< ICommand>();
+
+		 thread = new InvokerThread();
+		 invokerRunning = true;
+		 thread.start();
+
 		 loadCommands();
 	}
 
@@ -24,6 +32,9 @@ public class CommandAPI  {
 			ICommand command = commands.get(commandName).newInstance();	
 			command.setFile(new File(path));
 			commandQueue.add(command);
+
+            // Wake up the invoker thread
+            thread.notify();
 		} else {
 			throw new Exception("Command does not exist.");
 		}
@@ -34,4 +45,30 @@ public class CommandAPI  {
 		commands.put(FileNameCommand.getCommandName(), FileNameCommand.class);
 	}
 
+	private void executeCommand(){
+		if(!commandQueue.isEmpty()){
+			ICommand cmdToExecute = commandQueue.poll();
+			cmdToExecute.execute();
+		}
+	}
+
+	private synchronized boolean isInvokerRunning(){
+		return invokerRunning;
+	}
+
+	private class InvokerThread extends Thread{
+
+		@Override
+		public void run() {
+			while (isInvokerRunning()){
+				executeCommand();
+
+                if(commandQueue.isEmpty()){
+                    try {
+                        this.wait();
+                    } catch (InterruptedException ignored) {}
+                }
+			}
+		}
+	}
 }
--- src/fileprocessor/model/FileNameCommand.java
+++ src/fileprocessor/model/FileNameCommand.java
@@ -3,16 +3,24 @@ package fileprocessor.model;
 import java.io.File;
 
 public class FileNameCommand implements ICommand {
-	private File file;
-	public static String getCommandName() {
-		return "Nom du Fichier";
-	}
-	public FileNameCommand(File file) {
+    private File file;
+
+    public static String getCommandName() {
+        return "Nom du Fichier";
+    }
+
+    public FileNameCommand(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public String execute() {
+        return "Nom du fichier = "+file.getName();
+    }
+
+    public void setFile(File file) {
 		this.file = file;
-	}
-	@Override
-	public String execute() {
-		return null;
-	}
-	public void setFile(File file) {};
+    }
+
+
 }
