4355ce5902157138f04405ae2744e54ffeee811e
David Livet <david.livet@orange.fr>
2015-10-08 14:33:39 -0400

    De-singleton

--- src/fileprocessor/controller/CommandAPI.java
+++ src/fileprocessor/controller/CommandAPI.java
@@ -5,29 +5,17 @@ import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.ArrayList;
 
-import fileprocessor.model.AbsolutePathCommand;
 import fileprocessor.model.FileNameCommand;
-import fileprocessor.model.FolderNameCommand;
 import fileprocessor.model.ICommand;
 
-public class CommandAPI  {
+public class CommandAPI extends Observable {
 	private Map<String, Class<? extends ICommand>> commands = new HashMap<>();
 	private Queue<ICommand> commandQueue = new ConcurrentLinkedQueue<>();
 	private boolean invokerRunning;
 	private static final Object MUTEX_THREAD = new Object();
 	private static final Object MUTEX_COMMANDS = new Object();
 
-	//Singleton
-	private static CommandAPI instance = null;
-
-    public synchronized static CommandAPI getInstance() {
-       if(instance == null) {
-          instance = new CommandAPI();
-       }
-       return instance;
-    }
-
-	private CommandAPI() {
+	public CommandAPI() {
 		 commands = new HashMap<>();
 		 commandQueue = new ConcurrentLinkedQueue<>();
 
@@ -38,7 +26,7 @@ public class CommandAPI  {
 		 // TODO : importer la liste de commande dynamiquement
 		 addCommandClass(FileNameCommand.getCommandName(),FileNameCommand.class);
 	}
-	
+
 	public ArrayList<String> getCommands() {
 		ArrayList<String> commandList = new ArrayList<String>();
 		for(String key : commands.keySet()) {
@@ -47,7 +35,7 @@ public class CommandAPI  {
 		return commandList;
 	}
 
-	public void addCommandToQueue(String commandName, String path, Observer response) throws Exception, InstantiationException, IllegalAccessException {
+	public void addCommandToQueue(String commandName, String path, Observer response) throws Exception {
 		synchronized (MUTEX_COMMANDS){
 			if (commands.containsKey(commandName)) {
 				// Init command
@@ -66,7 +54,6 @@ public class CommandAPI  {
 		synchronized (MUTEX_THREAD){
 			MUTEX_THREAD.notifyAll();
 		}
-		
 	}
 
 	public void addCommandClass(String commandName, Class<? extends ICommand> commandClass){
@@ -75,6 +62,8 @@ public class CommandAPI  {
 		}
 
 		 // TODO : informer la vue d'une nouvelle commande
+		this.setChanged();
+		this.notifyObservers();
 	}
 
 	public void removeCommandClass(String commandName) throws Exception {
@@ -88,6 +77,8 @@ public class CommandAPI  {
 		}
 
 		// TODO : informer la vue d'une nouvelle commande
+		this.setChanged();
+		this.notifyObservers();
 	}
 
 	private void executeCommand(){
@@ -99,13 +90,6 @@ public class CommandAPI  {
 
 	public synchronized boolean isInvokerRunning(){
 		return invokerRunning;
-	}
-
-	public synchronized void setInvokerRunning(boolean isRunning){
-		invokerRunning = isRunning;
-		synchronized (MUTEX_THREAD){
-			MUTEX_THREAD.notifyAll();
-		}
 	}
 
 	private class InvokerThread extends Thread{
